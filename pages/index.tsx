c7f2c430b01e74db8f738bf595e0351b1a7577cb
sjdthree <34969867+sjdthree@users.noreply.github.com>
2022-03-18 19:15:33 -0400

    fix case in index.tsx to Chat

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Chat from "./chat";
+import Chat from "./Chat";
 import { useConnection } from "@self.id/framework";
 import Connect from "../components/Header";
 import { useEffect } from "react";
