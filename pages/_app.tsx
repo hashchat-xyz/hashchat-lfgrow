fece91bdc1b8bfe447c48b95f5db22c2793446b3
Max <89795178+maxmandia@users.noreply.github.com>
2022-03-18 17:28:42 -0400

    Added max's work.

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+export default function Home() {
+  return (
+    <h1>hello</h1>
+  )
+}
