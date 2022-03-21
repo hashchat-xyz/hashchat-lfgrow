a1097ec81fe63d84b4d69f79785b10629fc91032
Cody Hatfield <cody.hatfield@me.com>
2022-03-21 11:36:57 -0700

    Fix injected export

--- pages/connect.tsx
+++ pages/connect.tsx
@@ -6,7 +6,7 @@ import Paper from "@mui/material/Paper";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { useWeb3React } from "@web3-react/core";
 
-export const injected = new InjectedConnector({
+const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42, 56, 97, 137, 1337],
 });
 
