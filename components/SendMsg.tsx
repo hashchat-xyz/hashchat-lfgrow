eb6f55850bec9ec0c2cd11204733134637c0c642
Cody Hatfield <cody.hatfield@me.com>
2022-03-23 10:51:58 -0700

    Use selfID hook instead of Core

--- components/SendMsg.tsx
+++ components/SendMsg.tsx
@@ -23,7 +23,7 @@ import {
 } from "@cbj/ceramic-append-collection/dist/index.js";
 import { useWeb3React } from "@web3-react/core";
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
-
+import { useSelfID } from "../src/hooks";
 
 const CHAIN = "polygon";
 
@@ -31,90 +31,90 @@ const CHAIN = "polygon";
 // const ceramic = new CeramicClient()
 
 export default function SendMsg() {
-    const { account } = useWeb3React();
-    const [messages, setMessages] = useState([] as any[]);
-    const [toAddr, setAddr] = useState("");
-    const [msg, setMsg] = useState("");
-    const [streamId, setStreamId] = useState("");
-    const [authState, authenticate] = useMultiAuth();
-    const core = useCore();
+  const { account } = useWeb3React();
+  const [messages, setMessages] = useState([] as any[]);
+  const [toAddr, setAddr] = useState("");
+  const [msg, setMsg] = useState("");
+  const [streamId, setStreamId] = useState("");
+  const { selfID, web3Provider } = useSelfID();
+
+  const msgwrite = async () => {
     const litNodeClient = new LitJsSdk.LitNodeClient();
 
-    const msgwrite = async () => {
+    await litNodeClient.connect();
 
-        await litNodeClient.connect();
+    const collection: Collection = (await AppendCollection.create(
+      selfID.client.ceramic,
+      {
+        sliceMaxItems: 256,
+      }
+    )) as Collection;
 
-        const collection: Collection = (await AppendCollection.create(
-            core.ceramic,
-            {
-            sliceMaxItems: 256,
-            }
-        )) as Collection;
-    
-        const accessControlConditions = setAccessControlConditions(toAddr);
-        const { encryptedString, symmetricKey } = await LitJsSdk.encryptString("");
-        // Encrypt collection stream ID using dag-jose
-        const encryptedStreamId = await encryptMsg(
-            { threadStreamId: collection.id.toString() },
-            symmetricKey
-        );
-        let authSig = await generateLitAuthSig(authState);
-    
-        const encryptedSymmetricKey = await litNodeClient.saveEncryptionKey({
-            accessControlConditions,
-            symmetricKey,
-            authSig,
-            chain: CHAIN,
-        });
-    
-        await encryptAndAddMessageToCollection(collection, msg, symmetricKey);
-    
-        const doc = await TileDocument.create(core.ceramic, {
-            accessControlConditions: accessControlConditions,
-            encryptedSymmetricKey: encodeb64(encryptedSymmetricKey),
-            encryptedStreamId: encryptedStreamId,
-        });
-        const _streamId = doc.id.toString();
-    
-        await postToInbox(toAddr, _streamId);
-    
-        setStreamId(_streamId);
-        console.log("setting streamId ", _streamId);
-        console.log("Collection: ", collection.id.toString());
-    };
+    const accessControlConditions = setAccessControlConditions(toAddr);
+    const { encryptedString, symmetricKey } = await LitJsSdk.encryptString("");
+    // Encrypt collection stream ID using dag-jose
+    const encryptedStreamId = await encryptMsg(
+      { threadStreamId: collection.id.toString() },
+      symmetricKey
+    );
+    let authSig = await generateLitAuthSig(web3Provider.provider);
 
-  
-  return ( 
+    const encryptedSymmetricKey = await litNodeClient.saveEncryptionKey({
+      accessControlConditions,
+      symmetricKey,
+      authSig,
+      chain: CHAIN,
+    });
+
+    await encryptAndAddMessageToCollection(collection, msg, symmetricKey);
+
+    const doc = await TileDocument.create(selfID.client.ceramic, {
+      accessControlConditions: accessControlConditions,
+      encryptedSymmetricKey: encodeb64(encryptedSymmetricKey),
+      encryptedStreamId: encryptedStreamId,
+    });
+    const _streamId = doc.id.toString();
+
+    await postToInbox(toAddr, _streamId);
+
+    setStreamId(_streamId);
+    console.log("setting streamId ", _streamId);
+    console.log("Collection: ", collection.id.toString());
+  };
+
+  return (
     <>
-    <TextField
+      <TextField
         autoFocus
         disabled={false}
         fullWidth
         id="toAddr"
         label="toAddr"
         onChange={(event: { target: { value: any } }) =>
-        setAddr(event.target.value)
+          setAddr(event.target.value)
         }
         placeholder="Address"
         type="text"
         value={toAddr}
         variant="standard"
-    />
-    <TextField
+      />
+      <TextField
         autoFocus
         disabled={false}
         fullWidth
         id="msg"
         label="msg"
-        onChange={(event: { target: { value: React.SetStateAction<string>; }; }) => setMsg(event.target.value)}
+        onChange={(event: {
+          target: { value: React.SetStateAction<string> };
+        }) => setMsg(event.target.value)}
         placeholder="Enter your message here"
         type="text"
         value={msg}
         variant="standard"
-    />
-    <Button color="primary" onClick={() => msgwrite()}>
-     ENCRYPT AND SEND
-    </Button>
+      />
+      <Button color="primary" onClick={() => msgwrite()}>
+        ENCRYPT AND SEND
+      </Button>
     </>
   );
 }
